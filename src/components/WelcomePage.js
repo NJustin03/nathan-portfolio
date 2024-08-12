638cea4c6ddd2a07f3a4445a2fc80dbddf49d11d
Nathan Justin <nathanjustin03@gmail.com>
2024-08-12 15:39:27 -0400

    Made Desktop and Mobile versions of the Welcome Section

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import pfp from '../images/NathanPFP.JPG';
 
@@ -7,19 +8,44 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 
 export default function WelcomePage(){
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        };
+    }, []);
+
+    const isMobile = width <= 768;
+
     return(
         <Container>
-            <Row>
-            <div className="AboutHolder">
-                    <Col xs={6} md={6}>
-                        <h1 className="Introduction">Welcome!</h1>
-                        <h2 className="Introduction">My name is Nathan</h2>
-                    </Col>
-                    <Col xs={6} md={6}>
-                        <Image src={pfp} roundedCircle fluid className="Image"/>
-                    </Col>
-                </div>
-            </Row>
+            {isMobile ? (
+                //Mobile View
+                <div className="AboutHolder-mobile">
+                    <h1 className="Introduction">Welcome!</h1>
+                    <h2 className="Introduction">My name is Nathan</h2>
+                    <Image src={pfp} roundedCircle fluid className="Image-mobile"/>
+                    </div>
+            ) : (
+                <Row>
+                <div className="AboutHolder">
+                        <Col xs={6} md={6}>
+                            <h1 className="Introduction">Welcome!</h1>
+                            <h2 className="Introduction">My name is Nathan</h2>
+                        </Col>
+                        <Col xs={6} md={6}>
+                            <Image src={pfp} roundedCircle fluid className="Image"/>
+                        </Col>
+                    </div>
+                </Row>
+            )}
+            
         </Container>
     );
 }
