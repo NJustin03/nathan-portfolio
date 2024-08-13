6501663069c2163f86043d7d3d1d5e9c7f542e56
Nathan Justin <nathanjustin03@gmail.com>
2024-08-13 11:31:28 -0400

    Temporary Skills Portion Done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from './components/Header';
 import WelcomePage from './components/WelcomePage';
+//import SkillsPage from './components/SkillsPage.js';
 
 function App() {
   return (
@@ -24,7 +25,17 @@ function App() {
 
       <WelcomePage></WelcomePage>
       <h1 className="Section">About</h1>
+      <p className="AboutParagraph">Lorem ipsum odor amet, consectetuer adipiscing elit. Augue praesent sit in odio vulputate vehicula. Aliquet sit penatibus dapibus mauris elementum mauris etiam metus! Massa dis felis netus et euismod imperdiet felis augue. Cras ullamcorper libero inceptos mus elementum torquent vitae. Parturient lobortis class dignissim per erat, ultricies tempus luctus.
+      </p>
       <h1 className="Section">Skills</h1>
+      <br></br>
+      <br></br>
+      <h2 className="Introduction">Programming Languages</h2>
+      <p className="Introduction">C/C++, C#, HTML, CSS, Java, JavaScript, Python</p>
+      <h2 className="Introduction">Frameworks/Libraries</h2>
+      <p className="Introduction">React, Bootstrap</p>
+      <h2 className="Introduction">Other Technologies</h2>
+      <p className="Introduction">Github/Github Desktop, Unity, Postman, mySQL, MongoDB, Firebase </p>
       <h1 className="Section">Projects</h1>
       <h1 className="Section">Contact</h1>
       
---
+++ src/components/SkillsPage.js
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+//import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+/*import Image from 'react-bootstrap/Image';
+import CImg from '../images/c-program-icon.png';
+import CppImg from '../images/c_6132222.png';
+import CsImg from '../images/c-sharp_6132221.png';
+import JSImg from '../images/js.png';
+import HTMLImg from '../images/html_1051277.png';
+import CSSImg from '../images/html_1051277.png';
+import PyImg from '../images/Python-logo.png';*/
+
+
+export default function SkillsPage(){
+    return(
+        <Container>
+            <Row>
+                <div className="IconHolder">
+                    
+                    {/*
+                    <Col xs={1} md={1}>
+                        <Image src={CImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={CppImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={CsImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={JSImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={HTMLImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={HTMLImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    <Col xs={1} md={1}>
+                        <Image src={PyImg} thumbnail fluid className="Icon"/>
+                    </Col>
+                    */}
+                </div>
+            </Row>
+        </Container>
+        
+    );
+}
