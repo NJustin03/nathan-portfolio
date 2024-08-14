24f3306c88ebd0cd1dba7f3e52488635008de0aa
Nathan Justin <nathanjustin03@gmail.com>
2024-08-14 15:20:31 -0400

    Minor Padding Changes on the SkillsSection

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -5,11 +5,11 @@ export default function SkillsPage(){
         <Container>
             <div className="IconHolder" style={{paddingBottom: '35%'}}>
                 <h3 className="Subheading">Languages</h3>
-                <img src="https://skillicons.dev/icons?i=c,cpp,cs,java,js,html,css,haskell,py" className='img-fluid d-block' style={{paddingBottom : '5%'}} alt="Skill Icons"/>
+                <img src="https://skillicons.dev/icons?i=c,cpp,cs,java,js,html,css,haskell,py" className='img-fluid d-block' alt="Skill Icons"/>
                 <h3 className="Subheading">Libraries/Frameworks</h3>
-                <img src="https://skillicons.dev/icons?i=react,bootstrap,express,nodejs" className="img-fluid d-block" style={{paddingBottom : '5%'}} alt="Skill Icons"/>
+                <img src="https://skillicons.dev/icons?i=react,bootstrap,express,nodejs" className="img-fluid d-block" alt="Skill Icons"/>
                 <h3 className="Subheading">Databases</h3>
-                <img src="https://skillicons.dev/icons?i=mysql,mongo,firebase" className="img-fluid d-block" style={{paddingBottom : '5%'}} alt="Skill Icons"/>
+                <img src="https://skillicons.dev/icons?i=mysql,mongo,firebase" className="img-fluid d-block" alt="Skill Icons"/>
                 <h3 className="Subheading">Other Technologies</h3>
                 <img src="https://skillicons.dev/icons?i=unity,git,github,vscode,visualstudio,postman,ubuntu" className="img-fluid d-block" style={{paddingBottom : '5%'}} alt="Skill Icons"/>
             </div>
