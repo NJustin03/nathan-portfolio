5d5e717d51d3700d144036ee59ff2e0b56fd41c3
Nathan Justin <nathanjustin03@gmail.com>
2024-08-15 17:16:40 -0400

    Getting Ready to Publish

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -12,8 +12,7 @@ export default function ProjectsPage(){
     const cardInfo = [
         {image: gesturescapeImg, title: "Gesturescape", text: "A 3D VR puzzle games that utilizes hand pose detection to trigger abilities to traverse through different levels.", dateCreated: "Unity, C#, Oculus SDK, VR", githubLink:"https://github.com/SkyDrqgon/Gesturescape-game"},
         {image: cARnivalImg, title: "cARnival", text: "A mobile application where users play different AR games to assist in learning different languages.", dateCreated: "Unity, AR Foundation, C#", githubLink:"https://github.com/SkyDrqgon/cARnival-app"},
-        {image: spacehavocImg, title: "Space Havoc", text: "A 3D Space Shooter inspired by old Atari and arcade games where you shoot aliens from your spaceship.", dateCreated: "C++, Raylib/RayGUI", githubLink:"https://github.com/RyanTurner02/space-havoc"},
-        {image: spacehavocImg, title: "Music App", text: "A simple music application with login and signup with Google that allows you to upload and play music.", dateCreated: "React, Express, MongoDB, Firebase, Nodejs", githubLink:"https://github.com/SkyDrqgon/MERN-Project"},    
+        {image: spacehavocImg, title: "Space Havoc", text: "A 3D Space Shooter inspired by old Atari and arcade games where you shoot aliens from your spaceship.", dateCreated: "C++, Raylib/RayGUI", githubLink:"https://github.com/RyanTurner02/space-havoc"},   
     ];
 
     const [currentPage, setCurrentPage] = useState(1);
@@ -24,10 +23,8 @@ export default function ProjectsPage(){
         const updateCardsPerPage = () => {
             if (window.innerWidth <= 1200) {
                 setCardsPerPage(1);
-            } else if (window.innerWidth <= 1440) {
+            } else{
                 setCardsPerPage(2);
-            } else if (window.innerWidth >= 1600){
-                setCardsPerPage(3);
             }
         };
 
